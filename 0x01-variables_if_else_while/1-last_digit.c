eb103be2597f70adc237711ab9d5b432a4104965
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-20 17:57:04 +0000

    Fixing script to print the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,19 @@ int main(void)
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
+	int last = n % 10;
 
-	if (n > 5)
+	if (last > 5)
 	{
-		printf("Last digit of %d is and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
-	else if (n == 0)
+	else if (last == 0)
 	{
-		printf("Last digit of %d is and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
-	else if (n < 6 || n != 0)
+	else if (last < 6 || last != 0)
 	{
-		printf("Last digit of %d is and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
 
 	return (0);
