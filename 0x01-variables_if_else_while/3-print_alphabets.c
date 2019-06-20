a4ea1cbc98b4f4c2786770030bf39878d7f3d670
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-20 20:29:14 +0000

    Adding script to print the alphabet in lower and then in upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+   */
+int main(void)
+{
+	char apl, APL;
+
+	for (apl = 'a'; apl <= 'z'; apl++)
+	{
+		putchar(apl);
+	}
+	for (apl = 'A'; apl <= 'Z'; apl++)
+	{
+		putchar(apl);
+	}
+	putchar('\n');
+
+return (0);
+}
+
+
