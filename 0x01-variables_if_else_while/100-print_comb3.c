25019fdaf9f340cd2d660467f62216c5d4ea3805
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-23 04:20:12 +0000

    Adding script to print numbers from 00 to 99 without repeat sequences and numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num, num2;
+
+	for (num = 0; num <= 9 ; num++)
+	{
+		num2 = num + 1;
+			while (num2 <= 9)
+			{
+				putchar (num + '0');
+				putchar (num2 + '0');
+				if ((num == 8) && (num2 == 9))
+				{
+					break;
+				}
+				else
+				{
+					putchar (',');
+					putchar (' ');
+				}
+				num2++;
+			}
+	}
+	putchar ('\n');
+return (0);
+}
