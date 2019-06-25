ebc34be8710f11b91ab361133cf8cfc314c0b724
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-25 20:24:04 +0000

    Adding script to natural number from to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - Entry point
+*@n: pass the parameter to review
+*
+* Return: Always 0 (Success)
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i == 98)
+			{
+				break;
+			}
+			else
+			{
+				printf(", ");
+			}
+		}
+		putchar ('\n');
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i == 98)
+			{
+				break;
+			}
+			else
+			{
+				printf(", ");
+			}
+		}
+		putchar ('\n');
+	}
+	else
+	{
+		printf ("98\n");
+	}
+}
