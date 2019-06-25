2535c45b0038552a412b008b3a06bc2f273be493
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-25 22:44:47 +0000

    Adding script to print table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,4 +7,33 @@
 */
 void times_table(void)
 {
+	int i, j, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			m = i * j;
+			if (m <= 9)
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(m + '0');
+			}
+			else if(m > 9)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(m/10 + '0');
+				_putchar(m % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
 
