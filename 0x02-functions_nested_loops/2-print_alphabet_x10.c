8d696bac2065230b65f07dd974a0fc35c45a5108
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-25 15:23:39 +0000

    Update holberton.h and adding script to print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * print_alphabet_x10 - Entry point
+  *
+  * Return: Always 0 (Success)
+   */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	while ( j < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		j++;
+		_putchar('\n');
+	}
+}
