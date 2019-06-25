6a7c7805e1555d816d195b2f232c315eaffac32d
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-25 19:15:19 +0000

    Adding script to print all hours of a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+* jack_bauer - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+void jack_bauer(void)
+{
+	int h, m, s, cs;
+
+	for (h = 48; h <= 50; h++)
+	{
+		for (m = 48; m <= 51; m++)
+		{
+			for (s = 48; s <= 53; s++)
+			{
+				for (cs = 48; cs <= 57; cs++)
+				{
+					_putchar(h);
+					_putchar(m);
+					_putchar(58);
+					_putchar(s);
+					_putchar(cs);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
+
