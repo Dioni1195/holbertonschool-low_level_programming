379267b4163dd74e0cb2209fe713db6b078ccc54
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-25 19:36:07 +0000

    Adding script to add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* add - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+return (c);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+* times_table - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+
