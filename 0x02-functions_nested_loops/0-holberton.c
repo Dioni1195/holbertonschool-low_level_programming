5e1a758c2443ca2ec37891963037b9ee7681b8c8
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-25 14:41:32 +0000

    Adding README, Header file and script

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char txt[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+	_putchar(txt[i]);
+	}
+	_putchar('\n');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this project is about Functions and nested loops in C language
