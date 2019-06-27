f7c97d4616d6cbc8a63d12f2270f4efecc48932b
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-27 19:56:20 +0000

    Adding program buzzFizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * main  - writes the character c to stdout
+  *
+  * Return: On success 1.
+  * On error, -1 is returned, and errno is set appropriately.
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+return (0);
+}
