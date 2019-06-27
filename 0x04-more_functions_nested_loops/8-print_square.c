e8d5eab2eb417ad772796d5aa885555efa46db84
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-27 18:16:57 +0000

    Adding function to print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_square - writes the character c to stdout
+ * @size: Size of the square
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
