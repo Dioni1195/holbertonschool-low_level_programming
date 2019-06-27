5484fbf5dd69cf09d21538ce2e2ffba47a05a378
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-27 15:08:47 +0000

    Adding function to multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* mul - writes the character c to stdout
+* @a: First number to multiply
+* @b: Second number to multiply
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+return (r);
+}
