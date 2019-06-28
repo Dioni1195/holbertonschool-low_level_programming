0b79f46bdba4d2f59e515596321c2e54dc02cfc4
Dioni1195 <dionisio.arango95@gmail.com>
2019-06-28 01:37:20 +0000

    Adding function to print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+* print_triangle - writes the character c to stdout
+* @size: Size of triangle
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+void print_triangle(int size)
+{
+	int i, j, num, esp;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			num = i + 1;
+			for (j = 0; j < size; j++)
+			{
+				esp = size - num;
+				while (esp > 0)
+				{
+					_putchar(32);
+					esp--;
+				}
+				while (num > 0)
+				{
+					_putchar(35);
+					num--;
+				}
+			}
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
+
