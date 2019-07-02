e14eeb8fc8b818e122bc92ca8bafbe2874831012
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-02 15:02:02 +0000

    Adding function to print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* print_rev - writes the character c to stdout
+* @s: The pointer that has the array
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+ */
+void print_rev(char *s)
+{
+	int wrd, i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	wrd = i;
+	while (wrd >= 0)
+	{
+		_putchar(s[wrd]);
+		wrd--;
+	}
+	_putchar(10);
+}
