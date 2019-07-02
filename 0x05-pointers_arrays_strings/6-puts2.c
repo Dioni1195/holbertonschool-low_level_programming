7922d196a4527e7ccff5d95907339cd49eaf7062
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-02 16:07:33 +0000

    Adding fucntion to print characters of two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* puts2 - Print only the characters of 2
+* @str: Array to be evaluated
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		++i;
+	}
+	_putchar(10);
+}
