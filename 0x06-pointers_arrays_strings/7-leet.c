eb55b100e68a9f5ab52fb1f4ec94854367ce20a1
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-05 00:05:44 +0000

    Adding function 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,7 +17,7 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; let[j] != '\0'; j++)
+		for (j = 0; j <= 4; j++)
 		{
 			if (s[i] == let[j][0] || s[i] == let[j][1])
 			{
