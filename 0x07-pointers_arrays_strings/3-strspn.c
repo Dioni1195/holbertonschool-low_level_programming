b2d0c2f7e32a3e18ececdfdb0e185f4498a4a4d3
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-08 23:44:28 +0000

    Fixing 3 V2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,18 +13,18 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-			for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
 			{
-				if (s[i] == accept[j])
-				{
-					++count;
-					break;
-				}
-				if (accept[j + 1] == '\0')
-				{
-					return (count);
-				}
+				++count;
+				break;
 			}
+			if (accept[j + 1] == '\0')
+			{
+				return (count);
+			}
+		}
 	}
 return (count);
 }
