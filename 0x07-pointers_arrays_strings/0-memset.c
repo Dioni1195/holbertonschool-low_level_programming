2a5fe648c40d847a8ce5ccd75d7c77cc791558e2
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-08 15:22:24 +0000

    Adding ReadMe, Header file and memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+
+/**
+  * _memset - Replace n bytes of memory with a constant char
+  * @s: The pointer
+  * @b: The constant char
+  * @n: Number of bytes to replace
+  *
+  * Return: On success 1.
+  * On error, -1 is returned, and errno is set appropriately.
+   */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x07-pointers_arrays_strings
+
+This project is about the usage of double pointers and multidimensional arrays
