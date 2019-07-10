1654092b4bc6db20fde8716b0a9f60afa3ff5b45
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-10 01:00:58 +0000

    Final Fix print_diag

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,27 +1,29 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-  * string_toupper - Change a character from an array
-  * @s: The array to be evaluated
+  * print_diagsums - sum a diag
+  * @a: The array to be evaluated
+  * @size: size of array
   *
   * Return: On success 1.
   * On error, -1 is returned, and errno is set appropriately.
    */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum, **p;
+	int i, j, leng, ord, max, sum1, sum2;
 
-	sum = 0;
-	p = &a;
-	for (i = 0; i < size; i++)
+	leng = size * size;
+	ord = size - 1;
+	max = size + 1;
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < leng; i += max)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				sum = 0 + p[i][j];
-			}
-		}
+		sum1 += a[i];
 	}
-	putchar(sum + '0');
+	for (j = ord; j < (leng - ord); j += ord)
+	{
+		sum2 += a[j];
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
