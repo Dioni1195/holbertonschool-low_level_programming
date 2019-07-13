3ce51489aaa6b350aace811f1bc0113e447adb59
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-13 18:28:47 +0000

    Adding Arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Calculate the lenght of a string
+* @argc: The count of the arguments
+* @argv: The vector of arguments
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int main (int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
