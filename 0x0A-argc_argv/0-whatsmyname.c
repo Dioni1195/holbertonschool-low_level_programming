ae62a8868ba95ef71e426d33bf14009ca8cbb14c
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-13 18:20:37 +0000

    Fix argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Calculate the lenght of a string
+* @argc: The count of arguments
+* @argv: The vector of arguments
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
