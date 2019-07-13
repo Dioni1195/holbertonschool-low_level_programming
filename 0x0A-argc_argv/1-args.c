55b4cd657f6dff2fce787aef35874a9b5eb65d25
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-13 18:31:37 +0000

    Fixing count of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int main (int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
