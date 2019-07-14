ee09dd731bc8382b86c812f8b7d11598b882a040
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-14 23:28:27 +0000

    Fixing chang V4

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,16 +11,17 @@ int change(int n)
 {
 	if (n == 0)
 		return (0);
-	if (n % 25 == 0)
-		return (change(n - 25) + 1);
-	else if (n % 10 == 0)
-		return (change(n - 10) + 1);
-	else if (n % 5 == 0)
-		return (change(n - 5) + 1);
-	else if (n % 2 == 0)
-		return (change(n - 2) + 1);
-	else if (n % 1 == 0)
-		return (change(n - 1) + 1);
+	else if (n / 25 > 0)
+		return (change(n % 25) + (n / 25));
+	else if (n / 10 > 0)
+		return (change(n % 10) + (n / 10));
+	else if (n / 5 > 0)
+		return (change(n % 5) + (n / 5));
+	else if (n / 2 > 0)
+		return (change(n % 2) + (n / 2));
+	else if (n / 1 > 0)
+		return (change(n % 1) + (n / 1));
+	return (0);
 }
 
 /**
@@ -41,15 +42,15 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	coins = atoi(*(argv + 1));
-	if (coins < 0)
-	{
-		printf("0\n");
-		return (1);
-	}
-	else
+	if (coins > 0)
 	{
 		con1 = change(coins);
 		printf("%d\n", con1);
 		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (1);
 	}
 }
