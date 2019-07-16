941464d6c337991a2f0f5576f0d34fd6639cbef6
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-16 15:45:07 +0000

    Adding function strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+* _strdup - Copy an array or return NULL
+* @str: The string to be copied
+*
+* Return: On success a pointer.
+* On error, NULL is returned.
+*/
+char *_strdup(char *str)
+{
+	int i;
+	char *buff = malloc(sizeof(str));
+
+	i = 0;
+	if (buff == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (str[i] != '\0')
+		{
+			buff[i] = str[i];
+			i++;
+		}
+		return (buff);
+	}
+}
+
