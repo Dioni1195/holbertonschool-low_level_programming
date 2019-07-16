233053611a2208d0f04b1e5ab53a334c9e0fa239
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-16 22:59:41 +0000

    Adding grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+
+/**
+  * _strlen - Calculate lenght
+  * @s: The array
+  *
+  * Return: A pointer.
+  * On error, retunr NULL.
+  */
+int _strlen(char *s)
+{
+		int i;
+
+		i = 0;
+		while (s[i] != '\0')
+			i++;
+		return (i);
+}
+/**
+* alloc_grid - Create a grid
+* @width: width of the array
+* @height: height of the array
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int **alloc_grid(int width, int height)
+{
+	int **p, i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(int *) * height);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		*(p + i) = malloc(sizeof(int *) * width);
+		if (*(p + i) == NULL)
+		{
+			while (i > 0)
+			{
+				i--;
+				free(*(p + i));
+			}
+			free(p);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
+
+
+
