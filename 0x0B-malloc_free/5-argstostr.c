837b5feead5d0b6137e64a15be04d0d13fb4e2ef
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-17 03:30:32 +0000

    Fix lenght

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -33,7 +33,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 		sum += _strlen(*(av + i));
-	p = malloc(sizeof(char) * (sum));
+	p = malloc(sizeof(char) * (sum + ac));
 	if (p == NULL)
 		return (NULL);
 	for (j = 0; j < ac; j++)
