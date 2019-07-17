9d279cc52f9fa97ae874e72be6bebab123bb6e17
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-17 02:07:47 +0000

    Adding FreeGrid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+  * free_grid - Frees a grid
+  * @grid: grid
+  * @height: height of the array
+  *
+  * Return: On success 1.
+  * On error, -1 is returned, and errno is set appropriately.
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(*(grid + i));
+	free(grid);
+}
