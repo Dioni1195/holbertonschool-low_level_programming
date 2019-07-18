489eacf13be7300c1d0af4db92b8036793b277f4
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-18 22:32:10 +0000

    Fix return PTR in new == old

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (old_size == new_size)
-		return (NULL);
+		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
