3303001d54cba09e4b0e06ed15795fa9660c7d40
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-22 19:38:30 +0000

    Adding new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *nd;
+
+	nd = malloc(sizeof(dog_t));
+	if (nd == NULL)
+		return (NULL);
+	nd->name = name;
+	nd->age = age;
+	nd->owner = owner;
+	return (nd);
+}
