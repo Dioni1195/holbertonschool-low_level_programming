a589bd4deffb0cf7254119c0f6956554254f1017
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-24 18:32:31 +0000

    Fix validation of array NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,9 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	if (cmp != NULL)
+	if (cmp != NULL && size > 0 && array != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
