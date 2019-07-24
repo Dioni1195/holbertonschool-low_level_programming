21fc33013e18137ff8c68f01613100f433f1c85d
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-24 16:09:37 +0000

    Fix Betty in print_name, Add array_iterator and update header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,11 @@
 
+/**
+  * print_name - Function to print a name
+  *@name: The array to print
+  *@f: The function used to print
+  *
+  * Return: Nothing
+  */
 
 void print_name(char *name, void (*f)(char *))
 {
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+  * array_iterator - Function that applis an action in each element of an array
+  *@array: Array to evaluate
+  *@size: Size of the array
+  *@action: Action to execute
+  *
+  * Return: Always 0 (Success)
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
