025e950c4fc1675de7bdd1308712044df5140ee8
Dioni1195 <dionisio.arango95@gmail.com>
2019-07-27 18:47:27 +0000

    Fixing all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,43 +13,41 @@ void print_all(const char * const format, ...)
 {
 	va_list prnt;
 	char *cmp;
-	int i = 0, tam = strlen(format);
+	int i = 0;
 
-	va_start(prnt, format);
-	while (i < tam)
+	while (format == NULL)
 	{
-		if (format[i] == 'c' || format[i] == 's' ||
-				format[i] == 'i' || format[i] == 'f')
-		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(prnt, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(prnt, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(prnt, double));
-					break;
-				case 's':
-					cmp = va_arg(prnt, char *);
-					if (cmp == NULL)
-					{
-						printf("(nil)");
-						break;
-					}
-					printf("%s", cmp);
-					break;
-			}
-		while (i != (tam - 1))
-		{
-			printf(", ");
-			break;
-		}
-		}
-		i++;
+		printf("\n");
+		return;
 	}
-	printf("\n");
-	va_end(prnt);
+	va_start(prnt, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(prnt, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(prnt, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(prnt, double));
+				break;
+			case 's':
+				cmp = va_arg(prnt, char *);
+				if (cmp == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", cmp);
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+		|| format[i] == 's') && format[i + 1] != '\0')
+			printf(", ");
+	i++;
+	}
+	printf("\n"), va_end(prnt);
 }
