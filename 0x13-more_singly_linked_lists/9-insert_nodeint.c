2abf11ee0096ecb3a4f48e9307ba715560dd9f6a
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-08 18:33:35 +0000

    Add Feature : Fix Feature

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,24 +14,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *cpy = *head, *newNode;
 	unsigned int i = 0, pre;
 
-	newNode = malloc(sizeof(listint_t));
-	if (!newNode)
-		return (NULL);
 	while (cpy)
 	{
 		if (i == idx)
 		{
+			newNode = malloc(sizeof(listint_t));
+			if (!newNode)
+				return (NULL);
 			temp = cpy->next;
 			pre = cpy->n;
 			cpy->next = newNode;
 			cpy->n = n;
 			newNode->n = pre;
 			newNode->next = temp;
-			return (*head);
+			return (newNode);
 		}
 		i++;
 		cpy = cpy->next;
 	}
-	free(newNode);
 	return (NULL);
 }
