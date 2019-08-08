1dbd1dd79ed6c8cb2a7398dd0078051c12a4dc9a
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-08 17:16:50 +0000

    Add Feature : Adding function pop_listin

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Frees the head node and return its information
+ * @head: The linked list
+ *
+ * Return: The integer store int the deleted node
+ * On error, nothing
+*/
+int pop_listint(listint_t **head)
+{
+	int info;
+	listint_t *newLink, *temp = *head;
+
+	newLink = temp->next;
+	info = temp->n;
+	free(temp);
+	*head = newLink;
+	return (info);
+}
