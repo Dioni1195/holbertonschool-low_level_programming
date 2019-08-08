50583c2ed2dda423deff08c4085e09b7498223f2
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-08 16:56:57 +0000

    Add feature : Adding function to frees a linked list by another way

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a linked list
+ * @head: The linked list
+ *
+ * Return: Nothing
+ * On error, nothing
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *freeNode, *temp = *head;
+
+	while (temp)
+	{
+		freeNode = temp->next;
+		free(temp);
+		temp = freeNode;
+	}
+	*head = NULL;
+}
