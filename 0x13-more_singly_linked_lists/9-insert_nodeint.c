d5ddea7e238b9be28cc298694fc59aba81758d34
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-12 16:04:11 +0000

    Fix Feature : Fixing case Head = NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,12 +14,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *tmp = *head, *newNode;
 
-	if (!head)
-		return (NULL);
 	newNode = malloc(sizeof(listint_t));
 	if (!newNode)
 		return (NULL);
 	newNode->n = n;
+	if (!head)
+	{
+		newNode->next = NULL;
+		*head = newNode;
+		return (*head);
+	}
 	if (idx == 0)
 	{
 		newNode->next = tmp->next;
