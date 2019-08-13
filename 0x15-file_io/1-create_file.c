33ebda23a67a3d82de47cbea2d84e6fd26fffd58
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-13 21:13:19 +0000

    Fix Feature : Fix assign permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	txt_len = _strlen(text_content);
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
