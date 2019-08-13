f6e6cff3be25c7a441b5def61560d5f9adac3711
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-13 21:42:26 +0000

    Fix Feature : Fix type of write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ size_t _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t wr;
+	ssize_t wr = 0;
 	size_t txt_len;
 
 	txt_len = _strlen(text_content);
