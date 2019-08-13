04cdfa00c96b7b8042d995b4753fee6d042d5e8e
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-13 22:01:19 +0000

    Fix Feature : Fix close if write fails

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	txt_len = _strlen(text_content);
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
