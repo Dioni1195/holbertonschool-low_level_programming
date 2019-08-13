5cd70a39801dc1563df4f6caf7e8a0db9357c710
Dioni1195 <dionisio.arango95@gmail.com>
2019-08-13 21:31:13 +0000

    Fix Feature : Fix permissions and order of open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	txt_len = _strlen(text_content);
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -40,6 +40,6 @@ int create_file(const char *filename, char *text_content)
 		if (wr == -1)
 			return (-1);
 	}
-	close (fd);
+	close(fd);
 	return (1);
 }
