0ccc71497f392d3155872ec48923986b4a6087dc
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-13 14:54:26 +0000

    Add Feature : Add function to add node at end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - Add node at the end
+ * @head: The list to verify
+ * @n: The constant to add at the node
+ *
+ * Return: Pointer to the list
+ * On error, nothing
+*/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *aux, *newNode;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = NULL;
+	if (!*head)
+	{
+		newNode->prev = NULL;
+		return (*head = newNode);
+	}
+	aux = *head;
+	while (aux)
+	{
+		if (!aux->next)
+		{
+			newNode->prev = aux;
+			aux->next = newNode;
+			break;
+		}
+		aux = aux->next;
+	}
+	return (*head);
+}
+
+
+
