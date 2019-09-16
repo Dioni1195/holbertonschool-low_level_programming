69301719ad32f05ad303dd2653ae0d36dd076515
Dioni1195 <dionisio.arango95@gmail.com>
2019-09-16 00:26:18 +0000

    Fix memory leaks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 	{
+		free(newNode);
 		newNode = add_dnodeint(h, n);
 		return (newNode);
 	}
