edb4fe92fa3519479f6cfd349b665c7a14aebd55
Dioni1195 <dionisio.arango95@gmail.com>
2019-10-10 19:17:09 +0000

    Add esssentials

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - Function to create a hash table
+*@size: Size of the array
+*
+* Return: Return a pointer to a hashTable, on error, return NULL
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	if (!hash_table->array)
+		return (NULL);
+	return (hash_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,21 @@
+# 0x1A-hash_tables
+
+
+## DESCRIPTION
+With this project we learn about the hash tables in C. How create, how use and how manage them.
+
+
+## FILES
+  - [x] 0-hash_table_create.c
+  - [x] 1-djb2.c
+  - [x] 2-key_index.c
+  - [x] 3-hash_table_set.c
+  - [x] 4-hash_table_get.c
+  - [x] 5-hash_table_print.c
+  - [x] 6-hash_table_delete.c
+  
+  
+
+ ## Authors
+| [Dionisio Arango](https://github.com/Dioni1195?tab=repositories) 
+ 
