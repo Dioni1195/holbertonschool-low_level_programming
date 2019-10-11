67e487742464f50183b94b7f4bdd1c80c701e305
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-11 16:06:41 -0500

    Fixing null cases

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *aux;
 	int flg = 0;
 
-	if (!key || strcmp(key, "") == 0)
+	if (!key || strcmp(key, "") == 0 || !ht || !value)
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[idx])
