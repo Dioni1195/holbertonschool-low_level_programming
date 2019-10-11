8414766b8b2ede552ebc001462e0c77842ba63d9
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-11 17:17:45 -0500

    Fixing NULL cases

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *aux;
 
+	if (!ht || !key || key = "")
+		return (NULL);
 	idx = key_index((unsigned char *)key, ht->size);
 	aux = ht->array[idx];
 	while (aux)
