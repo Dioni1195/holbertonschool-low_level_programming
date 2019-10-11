d91ad3f1d608a34f73235fc7632f78e54e7162cf
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-11 17:19:12 -0500

    Fix strcmp

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *aux;
 
-	if (!ht || !key || key = "")
+	if (!ht || !key || strcmp(key, "") == 0)
 		return (NULL);
 	idx = key_index((unsigned char *)key, ht->size);
 	aux = ht->array[idx];
