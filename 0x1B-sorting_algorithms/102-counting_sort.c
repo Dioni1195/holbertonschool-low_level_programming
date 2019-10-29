33973a63380558c592b25f9e27827566caf3b0ec
Holberton <holberton@Holbertons-iMac-61.local>
2019-10-29 15:50:21 -0500

    Fixing sorting

--- 0x1B-sorting_algorithms/102-counting_sort.c
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -30,18 +30,21 @@ void counting_sort(int *array, size_t size)
 		value_arr[j] = 0;
 		j++;
 	}
+	/* Counting the times of appareances */
 	for (i = 0; i < size; i++)
 	{
 		value = array[i];
 		value_arr[value] += 1;
 	}
+	/* Adding values */
 	for(j = 1; j < size_arr; j++)
 		value_arr[j] += value_arr[j - 1];
+	/* Assignning new positions */
 	for(i = 0; i < size; i++)
 	{
 		value = array[i];
 		idx = value_arr[value];
-		array[idx] = value;
+		array[idx - 1] = value;
 		value_arr[value] -= 1;
 	}
 	free(value_arr);
