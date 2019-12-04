21824c243d1f9c030d4ab2c9ff5808b56fde035b
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-04 17:26:45 +0000

    Fix NULL case

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -8,6 +8,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int flag_1 = 1, flag_2 = 1;
 
+	if (!tree)
+		return (0);
 	if (tree->left)
 		flag_1 = binary_tree_is_full(tree->left);
 	if (tree->right)
