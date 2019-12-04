92a6f61040bf178857b8b649fe669d3b1f0fce2a
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-04 17:24:38 +0000

    Fixing null case

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -8,6 +8,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
+	if (!tree)
+		return (0);
 	if (tree->left)
 		left = binary_tree_height(tree->left);
 	if (tree->right)
