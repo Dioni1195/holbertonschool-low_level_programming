4dbd9cadf9b783c78c43b638a5745ba75f5fe3d3
Dioni1195 <dionisio.arango95@gmail.com>
2019-12-04 20:18:13 +0000

    Fix travers and check

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_balance - Calculate the balance
+ * @tree: The binary tree
+ *Return: If the tree is balanced
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left = height_recur(tree->left);
+	if (tree->right)
+		right = height_recur(tree->right);
+	return (left - right);
+}
+
+/**
  * height_recur - Calculate the height of a node
  * @tree: The binary tree
  * Return: The height
@@ -44,17 +62,18 @@ int binary_tree_is_full(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left = 0, is_full, right = 0, is_balanced;
+	int left = 1, is_full, right = 1, is_balanced;
 
 	if (!tree)
 		return (0);
 	if (tree->left)
-		left = height_recur(tree->left);
+		left = binary_tree_is_perfect(tree->left);
 	if (tree->right)
-		right = height_recur(tree->right);
-	is_balanced = left - right;
+		right = binary_tree_is_perfect(tree->right);
+	is_balanced = binary_tree_balance(tree);
 	is_full = binary_tree_is_full(tree);
-	if (is_balanced == 0 && is_full == 1)
+	if ((is_balanced == 0 && is_full == 1) &&
+		(left = 1 && right = 1))
 		return (1);
 	return (0);
 }
